ca0b3f8f742c863bb4fa3490b81e65a85c0739ba
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-23 22:37:43 +0700

    import of UsrNv and NavLnk.vue to component, some styling

--- estate-agent-vue/src/components/Navbar.vue
+++ estate-agent-vue/src/components/Navbar.vue
@@ -5,43 +5,26 @@
         <img src="" alt="">
       </a>
     </div>
-    <div class="bg-gray-300 w-full">
-      <ul class="flex w-full items-center justify-center p-4 space-x-7">
-        <li class="hover:text-gray-700" v-for="link in links">
-          <a href="{{ link.href }}">{{ link.name }}</a>
-        </li>
-      </ul>
+    <div>
+      <UserNav></UserNav>
+      <NavLinks></NavLinks>
     </div>
+    
+    
   </nav>
 </template>
 
 <script>
+
+import UserNav from './UserNav.vue';
+import NavLinks from './NavLinks.vue'
+
 export default {
+  components: { UserNav, NavLinks },
   name: 'Navbar',
   data() {
     return {
-      links: [
-        {
-          name: 'Home',
-          href: '1'
-        },
-        {
-          name: 'Properties',
-          href: '2'
-        },
-        {
-          name: 'About',
-          href: '3'
-        },
-        {
-          name: 'Contact',
-          href: '4'
-        },
-        {
-          name: 'Book a viewing',
-          href: '5'
-        },
-      ]
+      
     }
   }
 }
