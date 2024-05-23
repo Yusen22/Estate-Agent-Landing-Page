30d12c79229295bc04ca8fd48a828e9e8d146787
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-24 00:37:07 +0700

    added image for main card bg

---
+++ estate-agent-vue/src/components/MainCard/MainCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <img src='/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/pexels-michal-knotek-164395-542403.jpg'/>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'MainCard'
+}
+</script>
