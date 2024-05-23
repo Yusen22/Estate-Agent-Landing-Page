5dbb35b01de175cb2151f625db70da2c38686b66
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-23 22:36:35 +0700

    created user nav section with data for links

---
+++ estate-agent-vue/src/components/UserNav.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="flex absolute right-52 top-1">
+      <div class="w-full min-w-full">
+        <ul class="flex items-start justify-end space-x-9 p-4 absolute">
+          <li class="text-sm text-nowrap" v-for="link in userLinks">
+            <a href="{{ link.href }}">{{ link.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+    data() {
+      return {
+        userLinks: [
+          {
+            name: 'My Account',
+            href: '1'
+          },
+          {
+            name: 'Search',
+            href: '2'
+          },
+        ]
+      }
+    }
+  }
+  </script>
+  
+  
+  <style scoped></style>
+  
