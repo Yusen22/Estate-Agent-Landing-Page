3f490c35ee4988b5d10f5d80dd021f5f80f8c028
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-23 23:12:45 +0700

    removed absolute positioning from user nav links

--- estate-agent-vue/src/components/UserNav.vue
+++ estate-agent-vue/src/components/UserNav.vue
@@ -1,7 +1,7 @@
 <template>
-    <nav class="flex absolute right-52 top-1">
-      <div class="w-full min-w-full">
-        <ul class="flex items-start justify-end space-x-9 p-4 absolute">
+    <nav class="flex">
+      <div class="w-full">
+        <ul class="flex items-start justify-end space-x-9 p-4">
           <li class="text-sm text-nowrap" v-for="link in userLinks">
             <a href="{{ link.href }}">{{ link.name }}</a>
           </li>
