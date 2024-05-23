33cff7c7a39b4fefd2d43b04156e515dac8f6802
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-24 00:06:26 +0700

    changed abs position of logo

--- estate-agent-vue/src/components/NavLogo.vue
+++ estate-agent-vue/src/components/NavLogo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <a class="">
-            <img class="size-28 p-4 absolute left-32 bg-yellow-300 shadow-md" src='/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/1024px-Logo_of_Hong_Kong_Housing_Authority.svg.png' alt="">
+            <img class="size-28 p-4 absolute left-44 bg-yellow-300 shadow-md" src='/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/1024px-Logo_of_Hong_Kong_Housing_Authority.svg.png' alt="">
         </a>
     </div>
 </template>
