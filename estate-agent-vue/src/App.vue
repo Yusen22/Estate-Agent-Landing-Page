5cf211709424796d309a15a3b728fecbd30f502e
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-24 15:46:36 +0700

    added MPSection and MyBtn to page

--- estate-agent-vue/src/App.vue
+++ estate-agent-vue/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Navbar from './components/Navbar/Navbar.vue';
 import MainCard from './components/MainCard/MainCard.vue';
+import MainPrimarySection from './components/MainPage/MainPrimarySection.vue'
+import MyButton from './components/Common/MyButton.vue'
 
 </script>
 
@@ -9,13 +11,14 @@ import MainCard from './components/MainCard/MainCard.vue';
 
     <Navbar></Navbar>
     <MainCard></MainCard>
+    <MainPrimarySection></MainPrimarySection>
 
   </div>
 </template>
 
 <script>
 export default {
-  components: {Navbar, MainCard}
+  components: {Navbar, MainCard, MainPrimarySection, MyButton}
 }
 </script>
 
