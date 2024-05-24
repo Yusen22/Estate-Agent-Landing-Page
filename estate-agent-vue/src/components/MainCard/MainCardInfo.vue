71ce081e06a4eb94587578ae6f13670d02adfe22
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-24 15:51:54 +0700

    added MainCardInfo section with text

---
+++ estate-agent-vue/src/components/MainCard/MainCardInfo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="z-15 text-white text-center absolute top-56 right-0 left-0">   
+        <h1 class="text-6xl">{{ cardHeader }}</h1>
+        <p class="py-10">{{ cardSubtitle }}</p>
+        <MyButton :buttonText="cardLinkText"></MyButton>
+        
+    </div>
+</template>
+
+<script>
+import MyButton from '../Common/MyButton.vue'
+
+export default {
+    name: 'MainCardInfo',
+    components: { MyButton },
+    data() {
+        return {
+            cardHeader: 'Find a place you can call a home',
+            cardSubtitle: 'With over 2000 properties for rent & sale, our experts will take the trouble out of moving house',
+            cardLinkText: 'Get started today'
+        }
+    }
+}
+
+
+
+
+</script>
