44b68f67dc2dc5a1fa16da61b5c156442b25a616
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-24 15:48:36 +0700

    added z index to navbar. added handleScroll fn

--- estate-agent-vue/src/components/Navbar/Navbar.vue
+++ estate-agent-vue/src/components/Navbar/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="flex relative">
-    <div class="w-11/12 fixed top-0 left-0 right-0 text-white drop-shadow-md">
+  <nav class="flex relative z-10">
+    <div @scroll="handleScroll" :class="scrolled ? 'bg-white text-black': ''" class="w-11/12 fixed top-0 left-0 right-0 text-white drop-shadow-md">
       <NavLogo></NavLogo>
       <UserNav></UserNav>
       <NavLinks></NavLinks>
@@ -11,20 +11,30 @@
 </template>
 
 <script>
-
+import {ref, onBeforeMount} from 'vue'
 import UserNav from '../Navbar/UserNav.vue';
 import NavLinks from '../Navbar/NavLinks.vue'
 import NavLogo from '../Navbar/NavLogo.vue'
+
+
 
 export default {
   components: { UserNav, NavLinks, NavLogo},
   name: 'Navbar',
   data() {
     return {
+      scrolled: false
+    }
+  },
+  methods: {
+    handleScroll() {
       
+        console.log('ye')
     }
   }
 }
+
+
 </script>
 
 
