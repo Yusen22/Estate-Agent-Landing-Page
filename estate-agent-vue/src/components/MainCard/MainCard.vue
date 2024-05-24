c69723a491fa8a6ddfa606c3eb9295251867f349
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-24 15:52:07 +0700

    chnaged main card image and made bg

--- estate-agent-vue/src/components/MainCard/MainCard.vue
+++ estate-agent-vue/src/components/MainCard/MainCard.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-        <img src='/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/pexels-michal-knotek-164395-542403.jpg'/>
-    </div>
-    
+        <div class="bg-[url('src/assets/flat-front-with-garden.jpeg')] bg-cover h-[38rem] bg-center hue-rotate-60 brightness-50">
+            <div class="h-full w-full bg-"></div>
+        </div>
+        <MainCardInfo></MainCardInfo>
 </template>
 
 <script>
+import imageURL from '../../assets/pexels-michal-knotek-164395-542403.jpg'
+import MainCardInfo from './MainCardInfo.vue'
+
+
 export default {
-    name: 'MainCard'
+    name: 'MainCard',
+    components: { MainCardInfo }
 }
+
 </script>
