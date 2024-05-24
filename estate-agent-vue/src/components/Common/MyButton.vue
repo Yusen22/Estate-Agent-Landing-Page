c2887367d21b075fe7167689d4a7826bf9a641d1
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-24 15:47:02 +0700

    created MyButton component

---
+++ estate-agent-vue/src/components/Common/MyButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <button class="rounded-lg bg-yellow-400 p-5 shadow-lg">
+        {{ buttonText }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'MyButton',
+        props: ['buttonText']
+    }
+</script>
