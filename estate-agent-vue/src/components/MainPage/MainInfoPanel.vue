8214149cc6dfb46625bc5421af4f787a4217551b
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-26 14:36:33 +0700

    added styling and props for info panel

--- estate-agent-vue/src/components/MainPage/MainInfoPanel.vue
+++ estate-agent-vue/src/components/MainPage/MainInfoPanel.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-
+    <div class="info-panel text-center flex flex-col items-center p-6">
+        <h3 class="text-2xl uppercase">{{ mainText }}</h3>
+        <p>{{ subText }}</p>
+        <a href="{{ linkAddress }}">{{ linkText }}</a>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'MainInfoPanel'
+        name: 'MainInfoPanel',
+        props: ['mainText', 'subText', 'linkText', 'linkAddress']
     }
 
 </script>
