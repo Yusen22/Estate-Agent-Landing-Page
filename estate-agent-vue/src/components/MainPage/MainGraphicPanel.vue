02aa4dda18d197d8e6a060400578e1357541ab4b
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-26 15:52:19 +0700

    added styling to MGpanel. removed data

--- estate-agent-vue/src/components/MainPage/MainGraphicPanel.vue
+++ estate-agent-vue/src/components/MainPage/MainGraphicPanel.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="p-6 flex flex-col items-center graphic-panel">
-        <img :src="img1" class="bg-cover" />
-        <div class="flex flex-col items-center">
+    <div class="flex flex-col h-full items-center graphic-panel bg-stone-500">
+        <img :src="img1" class="object-cover" />
+        <div class="flex flex-col items-center p-5">
             <h3 class="text-2xl uppercase text center">{{ mainText }}</h3>
             <p class="text-center">{{ subText }}</p>
         </div>
@@ -14,9 +14,6 @@ export default {
     props: ['img1', 'mainText', 'subText'],
     data() {
         return {
-            imageProps: {
-                backgroundImage: "url('../')"
-            }
         }
     }
 
