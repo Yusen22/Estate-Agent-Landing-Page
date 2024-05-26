0f5935b8eabe42233d2e9b0a852ffb0fb2ab23b8
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-26 15:51:19 +0700

    added 2ndCard component

---
+++ estate-agent-vue/src/components/SecondCard/SecondaryCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="`w-full my-10 bg-[url('./assets/house-roof.jpeg')] h-[32rem] bg-bottom bg-cover`">
+        <div>
+            <h3></h3>
+            <p></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SecondaryCard',
+    props: ['secondCardImg'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
