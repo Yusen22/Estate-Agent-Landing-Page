0c2fdf802e2a7fe2402fb6f17726eafee993f261
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-27 15:18:39 +0700

    added fixed height to graph panel imgs

--- estate-agent-vue/src/components/MainPage/MainGraphicPanel.vue
+++ estate-agent-vue/src/components/MainPage/MainGraphicPanel.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="flex flex-col h-full items-center graphic-panel bg-stone-500">
-        <img :src="img1" class="object-cover" />
-        <div class="flex flex-col items-center p-5">
+    <div class="flex flex-col h-full items-center justify-between graphic-panel bg-stone-700">
+        <div class="relative">
+            <img :src="img1" class="h-[27rem] object-fill" />
+            <div class="absolute inset-0 bg-gradient-to-t from-stone-700 via-transparent to-transparent z-4"></div>
+        </div>
+        <div class="flex flex-col items-center justify-end p-5 mb-5 h-32 text-white">
             <h3 class="text-2xl uppercase text center">{{ mainText }}</h3>
             <p class="text-center">{{ subText }}</p>
         </div>
