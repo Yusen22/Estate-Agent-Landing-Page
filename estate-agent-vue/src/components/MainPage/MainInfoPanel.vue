8e7ec0894618920d31cb9afb201e4774a38f7c4c
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-27 15:18:58 +0700

    added bg-white to info panels

--- estate-agent-vue/src/components/MainPage/MainInfoPanel.vue
+++ estate-agent-vue/src/components/MainPage/MainInfoPanel.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="info-panel h-full text-center flex flex-col justify-around items-center p-6">
+    <div class="info-panel h-full text-center flex flex-col justify-around items-center p-6 bg-white">
         <h3 class="text-6xl uppercase">{{ mainText }}</h3>
         <p class="p-8">{{ subText }}</p>
         <a class="text-red-600 font-bold uppercase" href="{{ linkAddress }}">{{ linkText }}</a>
