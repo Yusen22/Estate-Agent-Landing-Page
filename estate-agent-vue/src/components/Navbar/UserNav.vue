3663cec8e1d8442668d9785c7f4f4144aca81f93
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-28 14:02:19 +0700

    imported and generated icons for userNav

--- estate-agent-vue/src/components/Navbar/UserNav.vue
+++ estate-agent-vue/src/components/Navbar/UserNav.vue
@@ -1,9 +1,10 @@
 <template>
     <nav class="flex">
       <div class="w-full">
-        <ul class="flex items-start justify-end space-x-9 p-4">
-          <li class="text-sm text-nowrap" v-for="link in userLinks">
+        <ul class="flex items-start justify-end space-x-3 p-4">
+          <li class="flex w-1/12 items-center justify-end text-sm text-nowrap" v-for="link in userLinks">
             <a href="{{ link.href }}">{{ link.name }}</a>
+            <component :is=link.icon class="ml-2 w-2/12"></component>
           </li>
         </ul>
       </div>
@@ -11,19 +12,30 @@
   </template>
   
   <script>
+  import {UserCircleIcon, Cog8ToothIcon, MagnifyingGlassCircleIcon} from '@heroicons/vue/24/solid';
+
+
   export default {
     name: 'Navbar',
+    components: {UserCircleIcon, Cog8ToothIcon, MagnifyingGlassCircleIcon},
     data() {
       return {
         userLinks: [
           {
             name: 'My Account',
-            href: '1'
+            href: '1',
+            icon: 'UserCircleIcon'
+          },
+          {
+            name: 'Settings',
+            href: '2',
+            icon: 'Cog8ToothIcon'
           },
           {
             name: 'Search',
-            href: '2'
-          },
+            href: '3',
+            icon: 'MagnifyingGlassCircleIcon'
+          }
         ]
       }
     }
