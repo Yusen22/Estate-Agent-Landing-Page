2bbdd65d7e5373d234371c2b1228a5762e4b942e
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-28 15:04:33 +0700

    added conditional style to border on scroll

--- estate-agent-vue/src/components/Navbar/UserNav.vue
+++ estate-agent-vue/src/components/Navbar/UserNav.vue
@@ -1,47 +1,55 @@
 <template>
-    <nav class="flex">
-      <div class="w-full">
-        <ul class="flex items-start justify-end space-x-3 p-4">
-          <li class="flex w-1/12 items-center justify-end text-sm text-nowrap" v-for="link in userLinks">
-            <a href="{{ link.href }}">{{ link.name }}</a>
-            <component :is=link.icon class="ml-2 w-2/12"></component>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </template>
-  
-  <script>
-  import {UserCircleIcon, Cog8ToothIcon, MagnifyingGlassCircleIcon} from '@heroicons/vue/24/solid';
+  <nav class="flex">
+    <div class="w-full">
+      <ul class="flex items-start justify-end space-x-1 py-4 transition">
+        <li v-for="(link, index) in userLinks" :key="index" :class="[
+          'flex w-1/12 items-center justify-center text-sm text-nowrap px-2',
+          { 'border-r': index !== userLinks.length - 1 },
+          { 'border-black': isScrolled }
+        ]">
+          <a href="{{ link.href }}">{{ link.name }}</a>
+          <component :is=link.icon class="ml-2 w-4"></component>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { UserCircleIcon, Cog8ToothIcon, MagnifyingGlassCircleIcon } from '@heroicons/vue/24/solid';
 
 
-  export default {
-    name: 'Navbar',
-    components: {UserCircleIcon, Cog8ToothIcon, MagnifyingGlassCircleIcon},
-    data() {
-      return {
-        userLinks: [
-          {
-            name: 'My Account',
-            href: '1',
-            icon: 'UserCircleIcon'
-          },
-          {
-            name: 'Settings',
-            href: '2',
-            icon: 'Cog8ToothIcon'
-          },
-          {
-            name: 'Search',
-            href: '3',
-            icon: 'MagnifyingGlassCircleIcon'
-          }
-        ]
-      }
+export default {
+  name: 'Navbar',
+  components: { UserCircleIcon, Cog8ToothIcon, MagnifyingGlassCircleIcon },
+  props: {
+    isScrolled: {
+      type: Boolean,
+      required: true
     }
-  }
-  </script>
-  
-  
-  <style scoped></style>
-  
+  },
+  data() {
+    return {
+      userLinks: [
+        {
+          name: 'My Account',
+          href: '1',
+        },
+        {
+          name: 'Settings',
+          href: '2',
+          icon: 'Cog8ToothIcon'
+        },
+        {
+          name: 'Search',
+          href: '3',
+          icon: 'MagnifyingGlassCircleIcon'
+        },
+      ],
+    }
+  }, 
+}
+</script>
+
+
+<style scoped></style>
