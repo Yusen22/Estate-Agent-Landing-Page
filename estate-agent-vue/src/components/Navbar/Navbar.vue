1e7db98ae16a30ba4a791fa427d16a5d66ffc554
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-28 15:05:20 +0700

    added props to UserNav

--- estate-agent-vue/src/components/Navbar/Navbar.vue
+++ estate-agent-vue/src/components/Navbar/Navbar.vue
@@ -1,17 +1,16 @@
 <template>
   <nav class="flex relative z-10">
-    <div @scroll="handleScroll" :class="bgColour ? 'bg-white !text-black': ''" class=" fixed top-0 left-0 right-0 text-white drop-shadow-md px-28">
-      <NavLogo></NavLogo>
-      <UserNav></UserNav>
+    <div :class="bgColour ? 'bg-white !text-black' : ''"
+      class="fixed transition top-0 left-0 right-0 text-white drop-shadow-md px-28">
+      <NavLogo ></NavLogo>
+      <UserNav :isScrolled="bgColour"></UserNav>
       <NavLinks></NavLinks>
     </div>
-    
-    
   </nav>
 </template>
 
 <script>
-import {ref, onBeforeMount} from 'vue'
+
 import UserNav from '../Navbar/UserNav.vue';
 import NavLinks from '../Navbar/NavLinks.vue'
 import NavLogo from '../Navbar/NavLogo.vue'
@@ -19,20 +18,14 @@ import NavLogo from '../Navbar/NavLogo.vue'
 
 
 export default {
-  components: { UserNav, NavLinks, NavLogo},
+  components: { UserNav, NavLinks, NavLogo },
   name: 'Navbar',
   data() {
     return {
-      scrolled: false
+  
     }
   },
   props: ['bgColour'],
-  methods: {
-    handleScroll() {
-      
-        console.log('ye')
-    }
-  }
 }
 
 
