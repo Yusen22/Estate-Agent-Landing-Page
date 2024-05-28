facb3c0d311a4cb500698f39b50710f97127033f
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-28 15:04:50 +0700

    contained in relative div

--- estate-agent-vue/src/components/Navbar/NavLogo.vue
+++ estate-agent-vue/src/components/Navbar/NavLogo.vue
@@ -1,18 +1,22 @@
 <template>
-    <div class="size-24 p-7 absolute left-40 bg-yellow-300 shadow-md">
-        <a class="class h-fit w-fit">
-            <img class="" src='/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/1024px-Logo_of_Hong_Kong_Housing_Authority.svg.png' alt="">
-        </a>
+    <div class="absolute">
+    <div class='relative h-[4rem] w-[4rem]'>
+        <div class="p-7 absolute min-w-full min-h-full left-40 bg-yellow-300 shadow-md">
+            <img class=""
+                src='/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/1024px-Logo_of_Hong_Kong_Housing_Authority.svg.png'
+                alt="">
+        </div>
+    </div>
     </div>
 </template>
 
 <script>
 export default {
-        name: 'NavLogo',
-        data () {
-            return {
-                imgUrl: '/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/1024px-Logo_of_Hong_Kong_Housing_Authority.svg.png'
-            }
+    name: 'NavLogo',
+    data() {
+        return {
+            imgUrl: '/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/1024px-Logo_of_Hong_Kong_Housing_Authority.svg.png'
         }
+    }
 }
 </script>
