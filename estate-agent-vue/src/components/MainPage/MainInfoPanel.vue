96f4a56bfe371b83926e3eb575c064494d397c4e
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-29 20:25:07 +0700

    imported PAirplane icon for Info panel

--- estate-agent-vue/src/components/MainPage/MainInfoPanel.vue
+++ estate-agent-vue/src/components/MainPage/MainInfoPanel.vue
@@ -2,14 +2,24 @@
     <div class="info-panel h-full text-center flex flex-col justify-around items-center p-6 bg-white">
         <h3 class="text-6xl uppercase">{{ mainText }}</h3>
         <p class="p-8">{{ subText }}</p>
-        <a class="text-red-600 font-bold uppercase" href="{{ linkAddress }}">{{ linkText }}</a>
+        <div class="flex relative">
+            <a class="relative right-[-2%] flex text-red-600 font-bold uppercase" href="{{ linkAddress }}">{{ linkText }}
+                <PaperAirplaneIcon class=" w-4 ml-2 left-56"></PaperAirplaneIcon>
+            </a>
+            
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'MainInfoPanel',
-        props: ['mainText', 'subText', 'linkText', 'linkAddress']
-    }
+import { PaperAirplaneIcon } from '@heroicons/vue/24/solid';
+
+
+
+export default {
+    name: 'MainInfoPanel',
+    props: ['mainText', 'subText', 'linkText', 'linkAddress'],
+    components: { PaperAirplaneIcon }
+}
 
 </script>
