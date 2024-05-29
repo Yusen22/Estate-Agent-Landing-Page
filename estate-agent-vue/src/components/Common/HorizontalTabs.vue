f51d4ef25d0bca96112c9d3c46e5d252f2aee293
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-29 20:21:40 +0700

    created HorizontalTabs component with styling

---
+++ estate-agent-vue/src/components/Common/HorizontalTabs.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="flex justify-center text-center border-b-2 border-gray-300 min-w-full min-h-min min-w-full mb-4">
+        <div 
+            v-for="(tab, index) in tabText" 
+            class="w-20 h-10" 
+            :class="{ 'bg-yellow-500/20 text-black border-b-4 border-yellow-500': activeTab === index, 'cursor-pointer': true }"
+            :key="index"
+            @click="handleActiveTab(index)"
+        >
+            <p class="py-1.5">{{ tab }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'HorizontalTabs',
+    props: {
+        tabText: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        handleActiveTab(index) {
+            this.activeTab = index
+        }
+    },
+    data() {
+        return {
+            activeTab: 0
+        }
+    }
+}
+
+
+</script>
