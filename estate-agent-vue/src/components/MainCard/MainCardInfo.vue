69903eb2818bc53742de764edfbf11614efc1fc0
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-29 20:24:44 +0700

    changed MainCardInfo font styling

--- estate-agent-vue/src/components/MainCard/MainCardInfo.vue
+++ estate-agent-vue/src/components/MainCard/MainCardInfo.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="z-15 text-white text-center absolute top-56 right-0 left-0">   
-        <h1 class="text-6xl">{{ cardHeader }}</h1>
-        <p class="py-10">{{ cardSubtitle }}</p>
-        <MyButton :buttonText="cardLinkText"></MyButton>
-        
+    <div class="z-15 text-white h-full text-center">   
+        <h1 class="text-6xl font-urbanist tracking-wide">{{ cardHeader }}</h1>
+        <p class="py-10 text-md">{{ cardSubtitle }}</p>
+        <!-- <MyButton :buttonText="cardLinkText"></MyButton> -->
     </div>
 </template>
 
@@ -16,7 +15,7 @@ export default {
     data() {
         return {
             cardHeader: 'Find a place you can call a home',
-            cardSubtitle: 'With over 2000 properties for rent & sale, our experts will take the trouble out of moving house',
+            cardSubtitle: 'With thousands of properties for rent & sale, our experts will take the trouble out of moving house',
             cardLinkText: 'Get started today'
         }
     }
