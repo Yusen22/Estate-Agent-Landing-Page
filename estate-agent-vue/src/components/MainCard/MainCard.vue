8eed5a27f45eeff8a80b8404988be29099a72154
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-30 00:04:54 +0700

    added searchInfo structure to MainCard

--- estate-agent-vue/src/components/MainCard/MainCard.vue
+++ estate-agent-vue/src/components/MainCard/MainCard.vue
@@ -2,9 +2,9 @@
     <div
         class="bg-[url('src/assets/flat-front-with-garden.jpeg')] bg-cover h-[47rem] bg-center hue-rotate-60 brightness-75">
     </div>
-    <div class="absolute top-64 right-0 left-0 flex flex-col items-center">
+    <div class="absolute top-60 right-0 left-0 flex flex-col items-center">
         <MainCardInfo></MainCardInfo>
-        <SearchBox :tabTextMaster='primSearchTabText'></SearchBox>
+        <SearchBox :tabTextMaster='primSearchTabText' :searchInfo="this.searchInfo"></SearchBox>
     </div>
 </template>
 
@@ -18,7 +18,19 @@ export default {
     components: { MainCardInfo, SearchBox },
     data() {
         return {
-            primSearchTabText: ['For sale', 'For rent', 'To sell']
+            primSearchTabText: ['For sale', 'For rent', 'To sell'],
+            searchInfo: [
+                {
+                    labelText: 'Search area',
+                    type: 'text',
+                    placeholder: 'e.g. London, New York, Phnom Penh'
+                },
+                {
+                    labelText: 'Budget',
+                    type: 'range',
+                    placeholder: ''
+                },
+            ]
         }
     }
 }
