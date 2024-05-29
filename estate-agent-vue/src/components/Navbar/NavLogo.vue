14d8f07dfa194a51a2b01ae263bbbeda6e62c33c
Elijah WIlsher <elijahwilsher@gmail.com>
2024-05-29 20:27:09 +0700

    got nav logo to fit in div, and added text

--- estate-agent-vue/src/components/Navbar/NavLogo.vue
+++ estate-agent-vue/src/components/Navbar/NavLogo.vue
@@ -1,13 +1,10 @@
 <template>
-    <div class="absolute">
-    <div class='relative h-[4rem] w-[4rem]'>
-        <div class="p-7 absolute min-w-full min-h-full left-40 bg-yellow-300 shadow-md">
-            <img class=""
+        <div class="p-9 absolute flex flex-col items-center justify-center max-w-full max-h-full left-40 bg-yellow-300 shadow-md">
+            <img class="size-10 object-center"
                 src='/Users/elijahwilsher/GITHUB REPOS/Estate-Agent-Landing-Page/estate-agent-vue/src/assets/1024px-Logo_of_Hong_Kong_Housing_Authority.svg.png'
                 alt="">
+            <h3 class="absolute font-sans text-xl tracking-widest bottom-0 uppercase font-bold py-1 !text-white">Logans</h3>
         </div>
-    </div>
-    </div>
 </template>
 
 <script>
